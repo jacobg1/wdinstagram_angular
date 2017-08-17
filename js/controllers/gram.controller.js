3b37bd2c0340fc35d6fa6a1b63d2748fea6132dd
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-17 13:56:10 -0400

    show partly working

--- js/controllers/gram.controller.js
+++ js/controllers/gram.controller.js
@@ -4,7 +4,14 @@ angular
   'Gram',
   GramIndexControllerFunction
 ])
-
+.controller('GramShowController', [
+  '$stateParams',
+  'Gram',
+  GramShowControllerFunction
+])
 function GramIndexControllerFunction (Gram) {
   this.grams = Gram.query()
+}
+function GramShowControllerFunction ($stateParams, Gram) {
+  this.gram = Gram.get({ id: $stateParams.id })
 }
