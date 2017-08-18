120eb242fb87fd5cd6127719428638a7fe558f14
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-17 20:19:13 -0400

    add edit and delete on show page

--- js/controllers/gram.controller.js
+++ js/controllers/gram.controller.js
@@ -1,4 +1,6 @@
-angular
+/* global angular */
+(function () {
+  angular
 .module('wdinstagram')
 .controller('GramIndexController', [
   '$state',
@@ -6,22 +8,36 @@ angular
   GramIndexControllerFunction
 ])
 .controller('GramShowController', [
+  '$window',
+  '$state',
   '$stateParams',
   'Gram',
   GramShowControllerFunction
 ])
-function GramIndexControllerFunction ($state, Gram) {
-  this.grams = Gram.query()
+  function GramIndexControllerFunction ($state, Gram) {
+    this.grams = Gram.query()
 
-  this.newGram = new Gram()
+    this.newGram = new Gram()
 
-  this.create = function () {
-    this.newGram.$save((newGram) => {
-      $state.go('gramShow', {id: newGram.id})
-    })
+    this.create = function () {
+      this.newGram.$save((newGram) => {
+        $state.go('gramShow', {id: newGram.id})
+      })
+    }
   }
-}
 
-function GramShowControllerFunction ($stateParams, Gram) {
-  this.gram = Gram.get({ id: $stateParams.id })
-}
+  function GramShowControllerFunction ($window, $state, $stateParams, Gram) {
+    this.gram = Gram.get({ id: $stateParams.id })
+  // put edit here too
+    this.edit = function () {
+      this.gram.$update({ id: $state.params.id }, () => {
+        $window.location.reload()
+      })
+    }
+    this.delete = function () {
+      this.gram.$delete({ id: $state.params.id }, (gram) => {
+        $state.go('gramIndex')
+      })
+    }
+  }
+})()
